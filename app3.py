ba18e1c6927a3117132bcbb040440e6e34e60abd
MarchellinC <marchellin2005@gmail.com>
2025-12-03 11:28:37 +0700

    tab 3

--- app3.py
+++ app3.py
@@ -377,7 +377,11 @@ st.markdown(
     }
 
     div[data-testid="stExpander"] > button > span:first-child {
-        display: contents !important; 
+        width: 0 !important;
+        margin: 0 !important;
+        padding: 0 !important;
+        min-width: 0 !important;
+        display: inline-block !important;
     }
 
     div[data-testid="stExpander"] {
